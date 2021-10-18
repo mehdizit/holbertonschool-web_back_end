9d5cc09583163d3deeec6dc602c34a5a1ad31423
mehdizit <zitounimehdi.it@gmail.com>
2021-10-18 22:30:03 +0100

    up

---
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+    Mixed Tuple
+"""
+from typing import Union, Tuple
+
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """
+        Args:
+            k: a String
+            v: Union: int or float
+        Return:
+            Tuple with string as first item and int or float as second item
+    """
+
+    #result: Tuple(str, Union[int, float])
+    result = (k, v**2)
+
+    return result
