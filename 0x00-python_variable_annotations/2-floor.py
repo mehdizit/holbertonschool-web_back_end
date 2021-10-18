dba31a0a20a801313e165389b48049a69fc16908
mehdizit <zitounimehdi.it@gmail.com>
2021-10-18 21:16:33 +0100

    up

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+import math
+"""handle float"""
+def floor(n: float):
+    """
+       Args:
+            n: float parameter
+       Return: floor of the float
+    """
+    return math.floor(n)
