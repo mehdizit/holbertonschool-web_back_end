7e5f24afae403346347070d4925939c1e5c9352e
mehdizit <zitounimehdi.it@gmail.com>
2021-10-18 21:00:53 +0100

    up

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""handle string annotation"""
+def concat(str1: str, str2: str) ->str:
+    """
+        Args:
+             str1: first parametre
+             str2: second parametre
+        Return:
+             concatenated string
+    """
+        
+    return(str1 + str2)
