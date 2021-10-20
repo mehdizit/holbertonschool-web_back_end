4e5ebae33bbaf36cc2287c29d2b7da79ef23b853
mehdizit <zitounimehdi.it@gmail.com>
2021-10-20 16:11:04 +0100

    up

---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+""""""
+
+import asyncio
+from typing import List
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """"""
+    delays: List[float] = []
+    all_delays: List[float] = []
+    for i in range(n):
+        delays.append(wait_random(max_delay))
+    for delay in asyncio.as_completed(delays):
+        earliest_result = await delay
+        all_delays.append(earliest_result)
+    return all_delays
---
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""Measures elapsed time"""
+
+import asyncio
+import random
+import time
+
+wait_n = __import__('1-concurrent_coroutines').wait_n
+
+def measure_time(n: int, max_delay: int) -> float:
+    """Returns total_time / n for wait_n() execution"""
+
+    elapsed_time: float
+    start_time = time.time()
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(wait_n(n, max_delay))
+    elapsed_time = time.time() - start_time
+    return elapsed_time / n
