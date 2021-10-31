8a576067da1680bb805699e98b9c857d77c89adb
mehdizit <zitounimehdi.it@gmail.com>
2021-10-31 22:03:31 +0100

    up

--- 0x05-personal_data/filtered_logger.py
+++ 0x05-personal_data/filtered_logger.py
@@ -1,14 +1,53 @@
 #!/usr/bin/env python3
-""" function that returns the log message obfuscated """
+""" RedactingFormatter """
 
-from typing import List
 import re
+from typing import List
+import logging
+
+PII_FIELDS = ('name', 'email', 'phone', 'ssn', 'password')
+
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        """ constructor """
+        self.fields = fields
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+
+    def format(self, record: logging.LogRecord) -> str:
+        """ generates a log"""
+        return filter_datum(self.fields, self.REDACTION, super().format(record), self.SEPARATOR)
+        
+
+def get_logger() -> logging.Logger:
+    """ returns a logging.Logger object """
+    logger = logging.getLogger("user_data")
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
+    handler = logging.StreamHandler()
+    handler.setFormatter(RedactingFormatter(PII_FIELDS))
+    logger.addHandler(handler)
+    return logger
 
 
 def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
-    """ returns the log message obfuscated """
+    """ function that return log message obfustucated """
+    lst = message.split(separator)
 
-    for item in fields:
-        message = re.sub(fr'{item}=.+?{separator}',
-                         f'{item}={redaction}{separator}', message)
-        return message
+    for f in fields:
+        for i in range(len(lst)):
+            if lst[i].startswith(f):
+                subst = f + '=' + redaction
+                lst[i] = re.sub(lst[i], '', lst[i])
+                lst[i] = subst
+    return separator.join(lst)
+
+
