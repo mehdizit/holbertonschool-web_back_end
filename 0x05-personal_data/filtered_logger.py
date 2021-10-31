563a68d03111dd49f6340b6c00f4c6a3271fe543
mehdizit <zitounimehdi.it@gmail.com>
2021-10-31 23:32:50 +0100

    up

--- 0x05-personal_data/filtered_logger.py
+++ 0x05-personal_data/filtered_logger.py
@@ -4,6 +4,8 @@
 import re
 from typing import List
 import logging
+import mysql.connector
+import os
 
 PII_FIELDS = ('name', 'email', 'phone', 'ssn', 'password')
 
@@ -50,4 +52,8 @@ def filter_datum(fields: List[str], redaction: str, message: str, separator: str
                 lst[i] = subst
     return separator.join(lst)
 
-
+def get_db() -> mysql.connector.connection.MySQLConnection:
+    """"""
+    cnx = mysql.connector.connect(user= os.getenv('PERSONAL_DATA_DB_USERNAME'), password= os.getenv('PERSONAL_DATA_DB_PASSWORD', ''), host= os.getenv('PERSONAL_DATA_DB_HOST', 'localhost'), database= os.getenv('PERSONAL_DATA_DB_NAME', 'root'))
+    return cnx
+    
