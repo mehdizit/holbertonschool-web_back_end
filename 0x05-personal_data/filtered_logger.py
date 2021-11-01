38677f32d07277b8de7e555c10727c6de8d4c9a7
mehdizit <zitounimehdi.it@gmail.com>
2021-11-01 21:02:04 +0100

    up

--- 0x05-personal_data/filtered_logger.py
+++ 0x05-personal_data/filtered_logger.py
@@ -52,7 +52,33 @@ def filter_datum(fields: List[str], redaction: str, message: str, separator: str
                 lst[i] = subst
     return separator.join(lst)
 
+
 def get_db() -> mysql.connector.connection.MySQLConnection:
     """returns a connector to the database"""
     cnx = mysql.connector.connect(user= os.getenv('PERSONAL_DATA_DB_USERNAME'), password= os.getenv('PERSONAL_DATA_DB_PASSWORD', ''), host= os.getenv('PERSONAL_DATA_DB_HOST', 'localhost'), database= os.getenv('PERSONAL_DATA_DB_NAME', 'root'))
     return cnx
+
+
+def main():
+    """ display each row under a filtered format """
+    db = get_db()
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM users;")
+    result = cursor.fetchall()
+    for row in result:
+        message = f"name={row[0]}; " + \
+                  f"email={row[1]}; " + \
+                  f"phone={row[2]}; " + \
+                  f"ssn={row[3]}; " + \
+                  f"password={row[4]};"
+        print(message)
+        log_record = logging.LogRecord("my_logger", logging.INFO,
+                                       None, None, message, None, None)
+        formatter = RedactingFormatter(PII_FIELDS)
+        formatter.format(log_record)
+    cursor.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    main()
