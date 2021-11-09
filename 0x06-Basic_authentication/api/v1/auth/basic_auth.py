87cd8e151cb0fea1406093b4a1700d8fde1d7cc6
mehdizit <zitounimehdi.it@gmail.com>
2021-11-10 00:41:52 +0100

    up

--- 0x06-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x06-Basic_authentication/api/v1/auth/basic_auth.py
@@ -42,7 +42,7 @@ class BasicAuth(Auth):
             return None, None
         if ':' not in decoded_base64_authorization_header:
             return None, None
-        credentials = decoded_base64_authorization_header.split(':')
+        credentials = decoded_base64_authorization_header.split(':', 1)
         return credentials[0], credentials[1]
     def user_object_from_credentials(self, user_email: str, user_pwd: str) -> TypeVar('User'):
         """ returns the User instance based on his email and password """
