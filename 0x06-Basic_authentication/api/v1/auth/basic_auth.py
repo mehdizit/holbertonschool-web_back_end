c7de431f2c925783380612ef02cf646642b2dea0
mehdizit <zitounimehdi.it@gmail.com>
2021-11-10 00:31:54 +0100

    up

--- 0x06-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x06-Basic_authentication/api/v1/auth/basic_auth.py
@@ -58,3 +58,14 @@ class BasicAuth(Auth):
         except Exception:
             return None
     
+    def current_user(self, request=None) -> TypeVar('User'):
+        """ overloads Auth and retrieves the User instance for a request """
+        try:
+            header = self.authorization_header(request)
+            base64Header = self.extract_base64_authorization_header(header)
+            decodeValue = self.decode_base64_authorization_header(base64Header)
+            credentials = self.extract_user_credentials(decodeValue)
+            user = self.user_object_from_credentials(credentials[0], credentials[1])
+            return user
+        except Exception:
+            return None
