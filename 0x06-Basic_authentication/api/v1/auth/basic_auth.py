29e423cfbf2beb71c38311e1cad915e8f5a651e6
mehdizit <zitounimehdi.it@gmail.com>
2021-11-09 17:54:34 +0100

    up

--- 0x06-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x06-Basic_authentication/api/v1/auth/basic_auth.py
@@ -31,3 +31,14 @@ class BasicAuth(Auth):
             return string_bytes.decode('utf-8')
         except Exception:
             return None
+
+    def extract_user_credentials(self, decoded_base64_authorization_header: str) -> (str, str):
+        """ returns tuple of email and password for users """
+        if decoded_base64_authorization_header is None:
+            return None, None
+        if not isinstance(decoded_base64_authorization_header, str):
+            return None, None
+        if ':' not in decoded_base64_authorization_header:
+            return None, None
+        credentials = decoded_base64_authorization_header.split(':')
+        return credentials[0], credentials[1]
