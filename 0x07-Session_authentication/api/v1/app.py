a1ce260c095900e4ab90825888f0c822de1847e3
mehdizit <zitounimehdi.it@gmail.com>
2021-11-10 23:21:33 +0100

    task_5

--- 0x07-Session_authentication/api/v1/app.py
+++ 0x07-Session_authentication/api/v1/app.py
@@ -54,9 +54,8 @@ def before_request() -> str:
                       '/api/v1/auth_session/login/']
     if not auth.require_auth(request.path, excluded_paths):
         return
-    if auth.authorization_header(request) is None:
-        abort(401)
-    if not auth.session_cookie(request):
+    if not auth.authorization_header(request)\
+       and not auth.session_cookie(request):
         abort(401)
     if auth.current_user(request) is None:
         abort(403)
