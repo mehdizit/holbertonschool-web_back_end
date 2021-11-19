9032ffbacd8b81a42d24a1d85e084c132a92f792
mehdizit <zitounimehdi.it@gmail.com>
2021-11-19 12:02:00 +0100

    task_6

---
+++ 0x08-user_authentication_service/app.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+""" set up a basic Flask app. """
+from flask import Flask
+
+@app.route('/', methods=['GET'], strict_slashes=False)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
