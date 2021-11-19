afb29ff80caa493ce2e2819465723449aec4441b
mehdizit <zitounimehdi.it@gmail.com>
2021-11-19 12:06:28 +0100

    task_6

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 """ set up a basic Flask app. """
-from flask import Flask
+from flask import Flask, jsonify
 
 @app.route('/', methods=['GET'], strict_slashes=False)
 def welcome():
     """ returns a message """
     return jsonify({"message": "Bienvenue"})
-    
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
