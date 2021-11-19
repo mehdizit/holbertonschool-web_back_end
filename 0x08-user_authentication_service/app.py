6bdf979c475deb9876ce96acd81d6ca060ef855a
mehdizit <zitounimehdi.it@gmail.com>
2021-11-19 12:18:28 +0100

    task_7

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -1,12 +1,27 @@
 #!/usr/bin/env python3
 """ set up a basic Flask app. """
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from auth import Auth
+
+
+AUTH = Auth()
 app = Flask(__name__)
 
 @app.route('/', methods=['GET'], strict_slashes=False)
 def welcome():
     """ returns a message """
     return jsonify({"message": "Bienvenue"})
+
+@app.route('/users', methods=['POST'], strict_slashes=False)
+def users():
+    """ register user """
+    email = request.form.get('email')
+    password = request.form.get('password')
+    try:
+        AUTH.register_user(email, password)
+        return jsonify({"email": email, "message": "user created"}), 200
+    except Exception:
+        return jsonify({"message": "email already registered"}), 400
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
