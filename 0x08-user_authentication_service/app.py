391fca6ec61244dd07d3153e948185f4609a788e
mehdizit <zitounimehdi.it@gmail.com>
2021-11-19 12:04:09 +0100

    task_6

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -3,5 +3,9 @@
 from flask import Flask
 
 @app.route('/', methods=['GET'], strict_slashes=False)
+def welcome():
+    """ returns a message """
+    return jsonify({"message": "Bienvenue"})
+    
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
