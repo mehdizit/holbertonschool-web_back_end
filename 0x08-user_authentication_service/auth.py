5ce63f7a91a717340af50328abfcc290d595da6d
mehdizit <zitounimehdi.it@gmail.com>
2021-11-20 11:23:18 +0100

    task_13

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -63,3 +63,10 @@ class Auth:
             return None
         else:
             return user
+    
+    def destroy_session(self, user_id: int) -> None:
+        """ updates the corresponding user’s session ID to None """
+        try:
+            self._db.update_user(user_id, session_id=None)
+        except NoResultFound:
+            return None
