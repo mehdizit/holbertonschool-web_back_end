0700145eeba4aaeed67aced7821f7d512c4a9c65
mehdizit <zitounimehdi.it@gmail.com>
2021-11-20 11:47:06 +0100

    task_17

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -54,6 +54,15 @@ def profile():
         abort(403)
     return jsonify({"email": user.email}), 200
 
+@app.route('/reset_password', methods=['POST'], strict_slashes=False)
+def get_reset_password_token():
+    """ generate a token and respond with a 200 HTTP status """
+    try:
+        email = request.form.get('email')
+        token = AUTH.get_reset_password_token(email)
+        return jsonify({"email": email, "reset_token": token}), 200
+    except ValueError:
+        abort(403)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
