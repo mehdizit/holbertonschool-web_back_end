e28aeb1716a7f438bedc518c8c6912e6158bd6e8
mehdizit <zitounimehdi.it@gmail.com>
2021-11-20 11:29:49 +0100

    task_14

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """ set up a basic Flask app. """
-from flask import Flask, jsonify, request, abort
+from flask import Flask, jsonify, request, abort, redirect
 from auth import Auth
 
 
