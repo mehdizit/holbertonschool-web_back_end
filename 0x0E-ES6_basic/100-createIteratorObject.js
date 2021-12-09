972b2985c8005369226a1e06912a41080cdd6010
mehdizit <zitounimehdi.it@gmail.com>
2021-12-09 22:49:48 +0100

    up

--- 0x0E-ES6_basic/100-createIteratorObject.js
+++ 0x0E-ES6_basic/100-createIteratorObject.js
@@ -1,3 +1,8 @@
 export default function createIteratorObject(report) {
-    return
+    const emp = [];
+    for (const k of Object.keys(report.allEmployees)) {
+      emp.push(...report.allEmployees[k]);
+    }
+    return emp;
   }
+  
